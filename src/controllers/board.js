ad32bf09f223f9cf7c594a7e5f70912a14ae0059
Nadezhda Komaritckaia <komaritskaya@Nadezhdas-MacBook-Pro.local>
2020-08-29 20:09:59 +0300

    Добавлена обработка ошибок и покачивание головой

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -164,6 +164,10 @@ export default class BoardController {
           this._showingTasksCount = this._showedTaskControllers.length;
 
           this._renderLoadMoreButton();
+        })
+        .catch(() => {
+          taskController.shake();
+
         });
       }
     } else if (newData === null) {
@@ -171,6 +175,10 @@ export default class BoardController {
         .then(() => {
           this._tasksModel.removeTask(oldData.id);
           this._updateTasks(this._showingTasksCount);
+        })
+        .catch(() => {
+          taskController.shake();
+
         });
     } else {
       this._api.updateTask(oldData.id, newData)
@@ -181,6 +189,9 @@ export default class BoardController {
             taskController.render(taskModel, TaskControllerMode.DEFAULT);
             this._updateTasks(this._showingTasksCount);
           }
+        })
+        .catch(() => {
+          taskController.shake();
         });
     }
   }
