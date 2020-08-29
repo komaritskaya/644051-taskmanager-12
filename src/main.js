803a08b569b4a8188eab5516f62b86e9e1da0167
Nadezhda Komaritckaia <komaritskaya@Nadezhdas-MacBook-Pro.local>
2020-08-29 19:39:43 +0300

    Добавление API

--- src/main.js
+++ src/main.js
@@ -1,32 +1,14 @@
+import API from "./api.js";
 import SiteMenuComponent, {MenuItem} from './components/site-menu';
 import FilterController from './controllers/filter';
 import BoardComponent from './components/board';
 import StatisticsComponent from './components/statistics';
 import TasksModel from './models/tasks';
 import BoardController from './controllers/board';
-import {generateTasks} from './mock/task';
 import {render} from './utils/render';
 
-const TASK_COUNT = 22;
-
-const tasks = generateTasks(TASK_COUNT);
-const tasksModel = new TasksModel();
-tasksModel.setTasks(tasks);
-
-const siteMainElement = document.querySelector(`.main`);
-const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
-
-const siteMenuComponent = new SiteMenuComponent();
-render(siteHeaderElement, siteMenuComponent);
-const filterController = new FilterController(siteMainElement, tasksModel);
-filterController.render();
-
-const boardComponent = new BoardComponent();
-
-render(siteMainElement, boardComponent);
-
-const boardController = new BoardController(boardComponent, tasksModel);
-boardController.render();
+const AUTHORIZATION = `Basic dXNlckBwYXNzd29yZAo=`;
+const END_POINT = `https://11.ecmascript.pages.academy/task-manager`;
 
 const dateTo = new Date();
 const dateFrom = (() => {
@@ -34,7 +16,23 @@ const dateFrom = (() => {
   d.setDate(d.getDate() - 7);
   return d;
 })();
+
+const api = new API(END_POINT, AUTHORIZATION);
+const tasksModel = new TasksModel();
+
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+
+const siteMenuComponent = new SiteMenuComponent();
 const statisticsComponent = new StatisticsComponent({tasks: tasksModel, dateFrom, dateTo});
+const boardComponent = new BoardComponent();
+const boardController = new BoardController(boardComponent, tasksModel, api);
+const filterController = new FilterController(siteMainElement, tasksModel);
+
+render(siteHeaderElement, siteMenuComponent);
+filterController.render();
+
+render(siteMainElement, boardComponent);
 render(siteMainElement, statisticsComponent);
 statisticsComponent.hide();
 
@@ -56,6 +54,13 @@ siteMenuComponent.setOnChange((menuItem) => {
       break;
   }
 });
+
+api.getTasks()
+  .then((tasks) => {
+    tasksModel.setTasks(tasks);
+    boardController.render();
+  });
+
 
 //    /\_/\
 //   / - - \
